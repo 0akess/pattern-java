e4826be0460bbf0fc3635b33b03ad4600f9a42d3
daniilsamsonenko <patrickdacker@gmail.com>
2022-08-31 19:07:16 +0300

    decorator fix

--- src/main/java/decorator/Run.java
+++ src/main/java/decorator/Run.java
@@ -15,26 +15,26 @@ public class Run {
         Sub sub = new ItalianBMT();
 
         sub = new Cheese(sub, 1);
-//        sub = new ChickenSteak(sub, 1);
-//        sub = new Olives(sub, .4);
+        sub = new ChickenSteak(sub, 1);
+        sub = new Olives(sub, .4);
         sub = new Cheese(sub, 6);
-//        sub = new ChickenSteak(sub, 2);
-//        sub = new Olives(sub, .2);
+        sub = new ChickenSteak(sub, 2);
+        sub = new Olives(sub, .2);
         sub = new Cheese(sub, 1);
 
         System.out.println(sub.cost());
         System.out.println(sub.getDescription());
 
-//        Sub sub2 = new Chicken();
-//
-//        sub2 = new Cheese(sub2, 12);
-//        sub2 = new ChickenSteak(sub2, 1);
-//        sub2 = new Olives(sub2, .4);
-//        sub2 = new Cheese(sub2, 4);
-//        sub2 = new ChickenSteak(sub2, 2);
-//        sub2 = new Olives(sub2, .2);
-//
-//        System.out.println(sub2.cost());
-//        System.out.println(sub2.getDescription());
+        Sub sub2 = new Chicken();
+
+        sub2 = new Cheese(sub2, 12);
+        sub2 = new ChickenSteak(sub2, 1);
+        sub2 = new Olives(sub2, .4);
+        sub2 = new Cheese(sub2, 4);
+        sub2 = new ChickenSteak(sub2, 2);
+        sub2 = new Olives(sub2, .2);
+
+        System.out.println(sub2.cost());
+        System.out.println(sub2.getDescription());
     }
 }
